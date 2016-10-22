94507cd6226e78a72e30d420ce5e8d4e05cbecf3
a-sep <artur.sep@gmail.com>
2016-10-22 09:13:52 +0200

    first events in js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,34 @@
 // ========= ver. 0.0  ===========
 // jshint ignore:line
 "use strict";
+let turnedOn = false;
+let strictMode = false;
+let strict = document.getElementById('strict');
+let switchButton = document.getElementsByClassName('switch');
+
+// wybierajac element po nazwie klasy otrzymujemy tablice elementow dlatego 'switchButton[0]'
+switchButton[0].addEventListener('click', function() {
+    if (!turnedOn) {
+        turnedOn = true;
+        this.style.left = '68px';
+        document.getElementById('count-number').style.color = '#ff0000';
+    } else {
+        turnedOn = false;
+        this.style.left = '90px';
+        document.getElementById('count-number').style.color = '#990000';
+
+    }
+    strictMode = false;
+    document.getElementById('strict-led').style.background = '#320000';
+});
+
+
+strict.addEventListener('click', function() {
+    if (turnedOn && !strictMode) {
+        strictMode = true;
+        document.getElementById('strict-led').style.background = '#ff0000';
+    } else if (turnedOn && strictMode) {
+        strictMode = false;
+        document.getElementById('strict-led').style.background = '#320000';
+    }
+});
